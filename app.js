e908d615671a6d13a7da970cfd0b2cdce561d3a4
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-07-02 18:02:49 +0700

    Edit icon

--- app.js
+++ app.js
@@ -30,13 +30,13 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Handlerbar Helpers
-const { formatDate, stripTags, truncate } = require('./helper/hbs');
+const { formatDate, stripTags, truncate, editIcon } = require('./helper/hbs');
 
 // Handlebars
 app.engine(
   '.hbs',
   exphbs({
-    helpers: { formatDate, stripTags, truncate },
+    helpers: { formatDate, stripTags, truncate, editIcon },
     defaultLayout: 'main',
     extname: '.hbs',
   })
@@ -56,6 +56,12 @@ app.use(
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
