8da9708eca5768115a1b27b4e65d86543234aec8
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-07-02 16:29:33 +0700

    dashboard time format

--- app.js
+++ app.js
@@ -29,8 +29,14 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Handlerbar Helpers
+const { formatDate } = require('./helper/hbs');
+
 // Handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine(
+  '.hbs',
+  exphbs({ helpers: { formatDate }, defaultLayout: 'main', extname: '.hbs' })
+);
 app.set('view engine', '.hbs');
 
 // Session
