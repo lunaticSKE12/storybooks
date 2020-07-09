8eee7993c31bbc5c75455553489e3604a4040abb
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-07-09 14:50:20 +0700

    Show story

--- routes/stories.js
+++ routes/stories.js
@@ -41,6 +41,25 @@ router.get('/', ensureAuth, async (req, res) => {
   }
 });
 
+// @desc  Show single story
+// @route Get /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate('user').lean();
+
+    if (!story) {
+      return res.render('error/404');
+    }
+
+    res.render('stories/show', {
+      story,
+    });
+  } catch (error) {
+    console.error(error);
+    res.render('error/404');
+  }
+});
+
 // @desc  Show edit page
 // @route Get /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
